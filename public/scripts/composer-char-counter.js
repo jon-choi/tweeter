72947976352e7948932763f89f49a15aa1ff32cd
Jonathan Choi <jonchoi11@gmail.com>
2021-08-11 00:00:51 +0000

    implemented character counter, adjusted counter code to turn red when limit is exceeded.

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+  let tweetMax = 140;
+  
+  $('textarea').input(function() {
+    let tweetLength= $(this).val().length;
+    let count = tweetMax - tweetLength;
+    
+    // traverses the DOM from to 'this' parent
+    // then searches children for a child with '.counter' class
+    // inputs the value of count into text field.
+
+    $(this)
+    .parent()
+    .children('.counter')
+    .text(count);
+
+    if (count < 0) {
+      $(this)
+      .parent()
+      .children('.counter')
+      .addClass('negative');
+    }
+
+    if (count >= 0) {
+      $(this)
+      .parent()
+      .children('.counter')
+      .removeClass('negative');
+    }
+  });
+});
