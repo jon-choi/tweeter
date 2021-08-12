082270ba1d907440a54984b631e167a7b78beab7
Jonathan Choi <jonchoi11@gmail.com>
2021-08-12 21:36:00 +0000

    lint changes

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,9 +4,7 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-$(document).ready(function() {
-
-
+$(document).ready(function () {
   ///--------> Prevents cross-site scripting <--------///
   const escape = function (str) {
     let div = document.createElement("div");
@@ -14,11 +12,11 @@ $(document).ready(function() {
     return div.innerHTML;
   };
 
-/// Returns html element from given object data
-  const createTweetElement = function(tweetObj) {
-  const $tweet = $('<article>').addClass('tweet');
+  /// Returns html element from given object data
+  const createTweetElement = function (tweetObj) {
+    const $tweet = $("<article>").addClass("tweet");
 
-  const html = `
+    const html = `
           <header>
             <img src= ${tweetObj.user.avatars}>
             <h4>${tweetObj.user.name}</h4>
@@ -33,66 +31,58 @@ $(document).ready(function() {
               <i class="far fa-heart"></i>
             </div>
           </footer>`;
-  
-  $tweet.append(html);
-  return $tweet;
-};
 
-// renders tweets by looping through array
-const renderTweets = function(tweetArr) {
-  for (const tweet of tweetArr) {
-    const $tweet = createTweetElement(tweet);
-    $('.tweet-container').prepend($tweet);
-  }
-};
+    $tweet.append(html);
+    return $tweet;
+  };
 
-// loads tweets from JSON
-const loadTweets = function() {
-  $.ajax('/tweets', { method: 'GET' })
-  .then(function (allTweets) {
-    renderTweets(allTweets);
+  // renders tweets by looping through array
+  const renderTweets = function (tweetArr) {
+    for (const tweet of tweetArr) {
+      const $tweet = createTweetElement(tweet);
+      $(".tweet-container").prepend($tweet);
+    }
+  };
+
+  // loads tweets from JSON
+  const loadTweets = function () {
+    $.ajax("/tweets", { method: "GET" }).then(function (allTweets) {
+      renderTweets(allTweets);
+    });
+  };
+  loadTweets();
+
+  // request to submit new tweets
+  $("#submit-form").submit(function (event) {
+    event.preventDefault();
+
+    const newTweetText = $(this).find("textarea").val();
+
+    if (!newTweetText) {
+      $("#noText").slideDown("slow");
+      setTimeout(() => {
+        $("#noText").slideUp("slow");
+      }, 2500);
+    } else if (newTweetText.length > 140) {
+      $("#tooLong").slideDown("slow");
+      setTimeout(() => {
+        $("#tooLong").slideUp("slow");
+      }, 2500);
+    } else {
+      const textStr = $("#submit-form").serialize();
+      $.post("/tweets/", textStr, function () {
+        loadTweets();
+      });
+
+      // empties text area after submission and counter goes back to 140
+      $("textarea").val("");
+      $(".counter").text("140");
+    }
   });
-};
-loadTweets();
 
-// request to submit new tweets
-$('#submit-form').submit(function(event) {
-  event.preventDefault();
-
-  const newTweetText = $(this).find('textarea').val();
-
-  if (!newTweetText) {
-    $('#noText')
-    .slideDown('slow')
-    setTimeout(() => {
-      $('#noText').slideUp('slow');
-    }, 2500)
-    
-  } else if (newTweetText.length > 140) {
-    
-    $('#tooLong')
-    .slideDown('slow')
-    setTimeout(() => {
-      $('#tooLong').slideUp('slow');
-    }, 2500)
-  
-  } else {
-  
-    const textStr = $('#submit-form').serialize();
-  $.post('/tweets/', textStr, function() {
-    loadTweets();
+  // hides or shows text area when button in header is clicked
+  $("#compose-new button").click(function () {
+    $(".new-tweet").slideToggle("slow");
+    $(".new-tweet textarea").focus();
   });
-
-  // empties text area after submission and counter goes back to 140
-  $('textarea').val('');
-  $('.counter').text('140');
-
-}
-});
-
-// hides or shows text area when button in header is clicked
-$('#compose-new button').click(function() {
-  $('.new-tweet').slideToggle('slow');
-  $('.new-tweet textarea').focus();
-})
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,31 +1,22 @@
-$(document).ready(function() {
+$(document).ready(function () {
   let tweetMax = 140;
-  
-  $('textarea').keydown(function() {
+
+  $("textarea").keydown(function () {
     let tweetLength = $(this).val().length;
     let count = tweetMax - tweetLength;
-    
+
     // traverses the DOM to 'this' parent
     // then searches children for a child with '.counter' class
     // inputs the value of count into text field.
 
-    $(this)
-    .parent()
-    .children('.counter')
-    .text(count);
+    $(this).parent().children(".counter").text(count);
 
     if (count < 0) {
-      $(this)
-      .parent()
-      .children('.counter')
-      .addClass('negative');
+      $(this).parent().children(".counter").addClass("negative");
     }
 
     if (count >= 0) {
-      $(this)
-      .parent()
-      .children('.counter')
-      .removeClass('negative');
+      $(this).parent().children(".counter").removeClass("negative");
     }
   });
 });
