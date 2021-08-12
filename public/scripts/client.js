b633d8c19971980c0ca2660a329b9be01dd734f9
Jonathan Choi <jonchoi11@gmail.com>
2021-08-12 00:10:36 +0000

    serialized form data with jquery, implemented load tweets function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,40 +6,40 @@
 
 $(document).ready(function() {
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    },
-    {
-      "user": {
-        "name": "Charlie",
-        "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@KittenMittens" },
-      "content": {
-        "text": "What is this word, ‘spa’? I feel like you’re starting to say a word and you’re not finishing it. Are you trying to say ‘spaghetti’? Are you taking me for a spaghetti day?"
-      },
-      "created_at": 1461116232227
-      }
-    ]
+  // const data = [
+  //   {
+  //     "user": {
+  //       "name": "Newton",
+  //       "avatars": "https://i.imgur.com/73hZDYK.png"
+  //       ,
+  //       "handle": "@SirIsaac"
+  //     },
+  //     "content": {
+  //       "text": "If I have seen further it is by standing on the shoulders of giants"
+  //     },
+  //     "created_at": 1461116232227
+  //   },
+  //   {
+  //     "user": {
+  //       "name": "Descartes",
+  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
+  //       "handle": "@rd" },
+  //     "content": {
+  //       "text": "Je pense , donc je suis"
+  //     },
+  //     "created_at": 1461113959088
+  //   },
+  //   {
+  //     "user": {
+  //       "name": "Charlie",
+  //       "avatars": "https://i.imgur.com/73hZDYK.png",
+  //       "handle": "@KittenMittens" },
+  //     "content": {
+  //       "text": "What is this word, ‘spa’? I feel like you’re starting to say a word and you’re not finishing it. Are you trying to say ‘spaghetti’? Are you taking me for a spaghetti day?"
+  //     },
+  //     "created_at": 1461116232227
+  //     }
+  //   ]
 
 
   const createTweetElement = function(tweetObj) {
@@ -72,6 +72,25 @@ const renderTweets = function(tweetArr) {
     $('.tweet-container').append($tweet);
   }
 };
-renderTweets(data);
+
+
+const loadTweets = function() {
+  $.ajax('/tweets', { method: 'GET' })
+  .then(function (allTweets) {
+    renderTweets(allTweets);
+  });
+};
+loadTweets();
+
+
+$('#submit-form').submit(function(event) {
+  event.preventDefault();
+  console.log(event);
+  const textStr = $('#submit-form').serialize();
+  $.post('/tweets/', textStr, function(data) {
+    console.log('data:', data);
+  })
+});
+
 
 });
