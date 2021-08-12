51f0915892d6c5cf54f9c8190379f35ac3030e2e
Jonathan Choi <jonchoi11@gmail.com>
2021-08-12 01:35:17 +0000

    validated forms so alerts show if no or too many characters are inputted. fetched tweets by submission and prepended so new tweets are shown at the top. added escape functionality to prevent cross-site scripting

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,47 +6,18 @@
 
 $(document).ready(function() {
 
-  // const data = [
-  //   {
-  //     "user": {
-  //       "name": "Newton",
-  //       "avatars": "https://i.imgur.com/73hZDYK.png"
-  //       ,
-  //       "handle": "@SirIsaac"
-  //     },
-  //     "content": {
-  //       "text": "If I have seen further it is by standing on the shoulders of giants"
-  //     },
-  //     "created_at": 1461116232227
-  //   },
-  //   {
-  //     "user": {
-  //       "name": "Descartes",
-  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
-  //       "handle": "@rd" },
-  //     "content": {
-  //       "text": "Je pense , donc je suis"
-  //     },
-  //     "created_at": 1461113959088
-  //   },
-  //   {
-  //     "user": {
-  //       "name": "Charlie",
-  //       "avatars": "https://i.imgur.com/73hZDYK.png",
-  //       "handle": "@KittenMittens" },
-  //     "content": {
-  //       "text": "What is this word, ‘spa’? I feel like you’re starting to say a word and you’re not finishing it. Are you trying to say ‘spaghetti’? Are you taking me for a spaghetti day?"
-  //     },
-  //     "created_at": 1461116232227
-  //     }
-  //   ]
+  const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
 
 
   const createTweetElement = function(tweetObj) {
   const $tweet = $('<article>').addClass('tweet');
 
   const html = `
-  <header>
+          <header>
             <img src= ${tweetObj.user.avatars}>
             <h4>${tweetObj.user.name}</h4>
             <p>${tweetObj.user.handle}</p>
@@ -69,7 +40,7 @@ $(document).ready(function() {
 const renderTweets = function(tweetArr) {
   for (const tweet of tweetArr) {
     const $tweet = createTweetElement(tweet);
-    $('.tweet-container').append($tweet);
+    $('.tweet-container').prepend($tweet);
   }
 };
 
@@ -85,11 +56,19 @@ loadTweets();
 
 $('#submit-form').submit(function(event) {
   event.preventDefault();
-  console.log(event);
+
+  const newTweetText = $(this).find('textarea').val();
+
+  if (!newTweetText) {
+    alert('Did you even type anything?');
+  } else if (newTweetText.length > 140) {
+    alert('Woah, settle down there! You went over the character limit!');
+  } else {
   const textStr = $('#submit-form').serialize();
-  $.post('/tweets/', textStr, function(data) {
-    console.log('data:', data);
+  $.post('/tweets/', textStr, function() {
+    loadTweets();
   })
+}
 });
 
 
